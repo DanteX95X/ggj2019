988637789fb00c63548dbdda4a3a13f3b053658c
dante <daniel.lewinski@silverbullet.pl>
2019-01-27 10:48:58 +0100

    Sound of footsteps.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -10,23 +10,37 @@ public class CharacterController : MonoBehaviour
 	private new Rigidbody2D rigidbody = null;
 	private SpriteRenderer sprite = null;
 	private AudioSource[] audios = null;
+	private AudioSource stepSounds = null;
 
 	private Vector2 targetPosition;
 	private Vector2 direction;
 
 	public float Speed => speed;
+
+	private float timer = 0.0f;
 	
 	private void Awake()
 	{
 		rigidbody = GetComponentInChildren<Rigidbody2D>();
 		sprite = GetComponentInChildren<SpriteRenderer>();
 		audios = GetComponents<AudioSource>();
+		stepSounds = sprite.GetComponent<AudioSource>();
 	}
 
 	private void Update()
 	{
 		if(Input.GetMouseButton(0))
 		{
+			if (timer > 0)
+			{
+				timer -= Time.deltaTime;
+			}
+			else
+			{
+				stepSounds.Play();
+				StepSoundsPlay();
+			}
+			
 			targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			if (targetPosition != (Vector2)transform.position)
 			{
@@ -37,11 +51,13 @@ public class CharacterController : MonoBehaviour
 		else
 		{
 			rigidbody.velocity = Vector2.zero;
+			timer = 0;
 		}
 
 		if ((targetPosition - (Vector2) transform.position).magnitude < 0.1f)
 		{
 			rigidbody.velocity = Vector2.zero;
+			timer = 0;
 		}
 		else
 		{
@@ -59,5 +75,10 @@ public class CharacterController : MonoBehaviour
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		audios[Random.Range(0, audios.Length)].Play();
+	}
+
+	private void StepSoundsPlay()
+	{
+		timer = 0.3f + Random.Range(0.0f, 0.1f);
 	}
 }
